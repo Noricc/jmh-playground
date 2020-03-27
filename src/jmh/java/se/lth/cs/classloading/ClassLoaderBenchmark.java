2a72289ff7955661a463e6bafbdf0b306f6130b3
Noric Couderc <noric.couderc@cs.lth.se>
2020-03-27 18:48:09 +0100

    Implemented loading benchmark information from JSON.
    The ClassLoaderBenchmark can load information about a benchmark from a
    JSON file.

--- src/jmh/java/se/lth/cs/classloading/ClassLoaderBenchmark.java
+++ src/jmh/java/se/lth/cs/classloading/ClassLoaderBenchmark.java
@@ -3,13 +3,17 @@
 // of the benchmark.
 package se.lth.cs.classloading;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
@@ -17,6 +21,7 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 public class ClassLoaderBenchmark {
 
@@ -28,19 +33,8 @@ public class ClassLoaderBenchmark {
      * @throws MalformedURLException
      * @throws ClassNotFoundException
      */
-    private static Class loadClassFromJar(String jarPath, String mainClass) {
-        if (jarPath == null) {
-            throw new IllegalArgumentException();
-        }
-        Path jar = Paths.get(jarPath).toAbsolutePath();
-
-        if (!jar.toFile().exists()) {
-            try {
-                throw new FileNotFoundException(jarPath);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
+    private static Class loadClassFromJar(String jarPath, String mainClass) throws FileNotFoundException {
+        Path jar = validatePath(jarPath);
 
         URLClassLoader loader = null;
         try {
@@ -61,12 +55,40 @@ public class ClassLoaderBenchmark {
         return classFromJar;
     }
 
+    private static Path validatePath(String jarPath) throws FileNotFoundException {
+        if (jarPath == null) {
+            throw new IllegalArgumentException();
+        }
+        Path jar = Paths.get(jarPath).toAbsolutePath();
+
+        if (!jar.toFile().exists()) {
+            throw new FileNotFoundException(jarPath);
+        }
+        return jar;
+    }
+
+    private static Map<String, Object> loadBenchmarkSpec(InputStream stream) {
+        JSONTokener tokener = new JSONTokener(stream);
+        JSONObject specification = new JSONObject(tokener);
+        return specification.toMap();
+    }
+
+    private static Map<String, Object> loadBenchmarkSpec(Path benchmarkSpecPath) throws FileNotFoundException {
+        FileInputStream fs = null;
+        fs = new FileInputStream(benchmarkSpecPath.toFile());
+        return loadBenchmarkSpec(fs);
+    }
+
+    public static Map<String, Object> benchmarksInfo;
+
     // We have a state which contains the main class of the program
-    @State(Scope.Thread)
+    @State(Scope.Benchmark)
     public static class BenchmarkState {
 
-        @Param("./programs/fop-all.jar")
-        public String programPath;
+        @Param("FOP")
+        public String benchmarkIdentifier;
+
+        public Map<String, Object> benchmarksInfo;
 
         Class classToLoad;
         String[] arguments = new String[] {} ;
@@ -75,27 +97,32 @@ public class ClassLoaderBenchmark {
         public BenchmarkState() {
         }
 
-        @Setup
-        public void doSetup() throws NoSuchMethodException {
-            classToLoad = loadClassFromJar(programPath, "org.apache.fop.cli.Main");
+        @Setup()
+        public void doSetup() throws NoSuchMethodException, FileNotFoundException {
+            Map<String, String> currentBenchmark = (Map<String, String>) benchmarksInfo.get(benchmarkIdentifier);
+            String jarPath = currentBenchmark.get("jar-path");
+            String mainClass = currentBenchmark.get("main-class");
+            classToLoad = loadClassFromJar(jarPath, mainClass);
             main = classToLoad.getDeclaredMethod("main", String[].class);
         }
     }
-
-
 
     @Benchmark
     public void runMain(BenchmarkState state) throws InvocationTargetException, IllegalAccessException {
         state.main.invoke(null, (Object) state.arguments);
     }
 
-    public static void main(String[] args) throws RunnerException {
+    public static void main(String[] args) throws RunnerException, FileNotFoundException {
+
+        benchmarksInfo = loadBenchmarkSpec(Paths.get(args[0]));
+
         Options opt = new OptionsBuilder()
                 .include(ClassLoaderBenchmark.class.getSimpleName())
                 .warmupIterations(5)
                 .measurementIterations(5)
                 .threads(4)
                 .forks(1)
+                .shouldFailOnError(true)
                 .build();
 
         new Runner(opt).run();
